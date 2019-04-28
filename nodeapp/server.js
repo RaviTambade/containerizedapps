12e4ed56ce5039575532ac07b9afe6f3028e0f2d
Ravi Tambade <ravi.tambade@transflower.in>
2019-04-28 09:27:41 +0530

    This is my container app

---
+++ nodeapp/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var app = express();
+var path=require("path");
+var fs=require("fs");
+
+
+//server configuration
+app.use(express.static(path.join(__dirname,'public')));
+
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/index.html'));
+});
+ 
+
+app.get('/hello', function (req, res) {
+  console.log("CAlling rest api");
+  var person={firstName:'Ravi',lastName:'Tambade',age:43};
+  res.send(person);
+});
+
+ 
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://localhost:8081", host, port)
+})
